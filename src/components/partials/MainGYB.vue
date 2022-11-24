1b6e1bd7e3d6b273920a9c912fa221193e0406e6
mikzanco <mikyzancolo97@gmail.com>
2022-11-24 20:26:56 +0100

    add and fix img on Maingyb

--- src/components/partials/MainGYB.vue
+++ src/components/partials/MainGYB.vue
@@ -6,8 +6,14 @@ export default {
 
 <template>
 
-    <div class="main2">
+    <div class="main2 d-flex align-items-center">
+        <div class="imgs">
+            <img class="front" src="../../assets/img/men-data.png" alt="">
+            <img class="back" src="../../assets/img/nemoodar.png" alt="">
+        </div>
+        <div class="text">
 
+        </div>
     </div>
   
 </template>
@@ -19,6 +25,25 @@ export default {
         width: 100vw;
         height: 500px;
         background: linear-gradient(185deg, rgba(82,124,235,1) 0%, rgba(0,0,0,1) 81%);
+        .imgs{
+            width: 50%;
+            height: 100%;
+            position: relative;
+            img.front{
+                height: 350px;
+                position: absolute;
+                right: 100px;
+                top: 70px;
+                z-index: 2;
+            }
+            img.back{
+                height: 350px;
+                position: absolute;
+                right: 150px;
+                top: 50px;
+                z-index: 1;
+            }
+        }
     }
 
 </style>
