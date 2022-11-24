62eb25a7577b11927a298dd76a66d2c599e4a925
mikzanco <mikyzancolo97@gmail.com>
2022-11-24 14:01:39 +0100

    Add header and create jumbotron and add jumbotron to appheader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,16 +1,70 @@
 <script>
+
+import Jumbotron from './jumbotron.vue'
+
 export default {
     name: 'AppHeader',
+    components:{
+        Jumbotron,
+    }
 }
 </script>
 
 <template>
-  <header>H</header>
+  <header>
+    <div class="container">
+        <div class="container-hea d-flex">
+            <div class="logo">
+                Logo
+            </div>
+            <div class="nav ">
+                <ul class="list d-flex">
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Services</li>
+                    <li>Blog</li>
+                    <li>Contact</li>
+                    <li>Portfolio</li>
+                    <button class="blue"><span>Sign In</span></button>
+                </ul>
+            </div>
+        </div>
+        <div class="container-jumbotron">
+            <Jumbotron/>
+        </div>
+        
+    </div>
+    
+  </header>
 </template>
 
 
 
 <style lang="scss" scoped>
     @use '../style/partials/variables' as *;
+    header{
+        height: 700px;
+        max-width: 100vw;
+        background: linear-gradient(185deg, rgba(82,124,235,1) 0%, rgba(0,0,0,1) 81%);
+        .container-hea{ 
+            justify-content: space-between;
+            .nav .list li{
+                padding: 20px;
+                list-style-type: none;
+
+            }
+            button{
+                height: 40px;
+                margin-top: 10px;
+                padding: 5px;
+                border-radius: 20px;
+                border: none;
+                background: linear-gradient(153deg, rgba(3,167,211,1) 0%, rgba(82,124,235,1) 60%);
+                span{
+                    padding: 10px;
+                }
+            }
+        }
+    }
 
 </style>
---
+++ src/components/jumbotron.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    name: 'Jumbotron',
+}
+</script>
+
+<template>
+  <div class="jumbotron">
+    <h1>jumbotron</h1>
+  </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+    h1{
+        color: white;
+    }
+</style>
