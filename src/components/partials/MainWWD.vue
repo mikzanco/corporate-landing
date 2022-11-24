217eb2aa7266b4fb28be2fbf918226de85a107eb
mikzanco <mikyzancolo97@gmail.com>
2022-11-24 19:24:16 +0100

    main 1 fatto, successivamente da ricreare in store un array per ricreare le quattro schede con un ciclo

--- src/components/partials/MainWWD.vue
+++ src/components/partials/MainWWD.vue
@@ -7,10 +7,32 @@ export default {
 <template>
     <div class="main1">
         <div class="container ">
-            <div class="d-flex flex-column justify-content-center align-items-center mt-3">
+            <div class="d-flex flex-column justify-content-center align-items-center mt-5">
                 <p class="red-text">Our Services</p>
-                <h2><span>What</span> We Do</h2>
-                <p>When, while the lovely valley teems with vapour around meand the</p>
+                <h2><strong>What</strong> We Do</h2>
+                <p class="gray-color mt-2">When, while the lovely valley teems with vapour around meand the</p>
+            </div>
+            <div class="functions d-flex p-5  justify-content-center">
+                <div class="col-3 justify-content-center m-2 p-4">
+                    <img src="../../assets/img/Group-247.png" alt="">
+                    <h3><strong>Data Analysis</strong></h3>
+                    <p class="gray-color mt-2 ">When, while the lovely valley teems with vapour around meand the meridian sun strikes the upper</p>
+                </div>
+                <div class="col-3 justify-content-center m-2 p-4">
+                    <img src="../../assets/img/Group-567.png" alt="">
+                    <h3><strong>Data Analysis</strong></h3>
+                    <p class="gray-color mt-2 ">When, while the lovely valley teems with vapour around meand the meridian sun strikes the upper</p>
+                </div>
+                <div class="col-3 justify-content-center m-2 p-4">
+                    <img src="../../assets/img/Group-538.png" alt="">
+                    <h3><strong>Data Analysis</strong></h3>
+                    <p class="gray-color mt-2 ">When, while the lovely valley teems with vapour around meand the meridian sun strikes the upper</p>
+                </div>
+                <div class="col-3 justify-content-center m-2 p-4">
+                    <img src="../../assets/img/Group-566.png" alt="">
+                    <h3><strong>Data Analysis</strong></h3>
+                    <p class="gray-color mt-2 ">When, while the lovely valley teems with vapour around meand the meridian sun strikes the upper</p>
+                </div>
             </div>
         </div>
     </div>
@@ -24,5 +46,23 @@ export default {
         width: 100vw;
         height: 800px;
         color: black;
+        h2{
+            font-size: 3rem;
+        }
+        .col-3{
+            height: 400px;
+            // background-color: red;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            img{
+                height: 200px;
+                width: 150px;
+                padding-bottom: 50px;
+            }
+            p{
+                text-align: center;
+            }
+        }
     }
 </style>
