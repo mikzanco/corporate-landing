af0ace535f4ff9174c6688c248b5987732635692
mikzanco <mikyzancolo97@gmail.com>
2022-11-24 12:17:24 +0100

    first commit. scafolding done

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    name: 'AppHeader',
+}
+</script>
+
+<template>
+  <header>H</header>
+</template>
+
+
+
+<style lang="scss" scoped>
+    @use '../style/partials/variables' as *;
+
+</style>
