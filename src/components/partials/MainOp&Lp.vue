479d4b5dfe575940129f64a572e1892f7a375d0f
mikzanco <mikyzancolo97@gmail.com>
2022-11-25 08:25:20 +0100

    create components and add per completare il componente MainOp&Lp

--- src/components/partials/MainOp&Lp.vue
+++ src/components/partials/MainOp&Lp.vue
@@ -1,16 +1,37 @@
 <script>
+
+import Op from './Op.vue'
+import Lp from './Lp.vue'
+
 export default {
     name: 'MainOp&Lp',
+    components:{
+        Op,
+        Lp,
+    }
 }
 </script>
 
 <template>
 
+    <div class="main4">
+        <Op/>
+        <div class="centerCard">
+        
+        </div>
+        <Lp/>
+    </div>
   
 </template>
 
 
 <style lang="scss" scoped>
     @use '../../style/partials/variables' as *;
+    .main4{
+        position: relative;
+        min-width: 100vw;
+        height: 1300px;
+        background-color: red;
+    }
 
 </style>
---
+++ src/components/partials/Op.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    name: 'Op',
+}
+</script>
+
+<template>
+
+
+  
+</template>
+
+
+<style lang="scss" scoped>
+    @use '../../style/partials/variables' as *;
+
+</style>
